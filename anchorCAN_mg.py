605c8518e83f1a83af3eca86e10fad569e70c205
pawneeranger <33731788+pawneeranger@users.noreply.github.com>
2020-02-04 10:23:10 +0800

    Changed functions name for clarity, started working with CAN library

--- anchorCAN_mg.py
+++ anchorCAN_mg.py
@@ -6,8 +6,9 @@ from Crypto.Cipher import AES
 from Crypto.Hash import HMAC, SHA256
 from Crypto.Util.strxor import strxor as xor
 from Crypto.Random import get_random_bytes
-    
-def sendCanFrame(anchor_random_number, message, can_id_key, can_id_counter, can_id_initial_vector):
+import can
+
+def generateAnchorFrame(anchor_random_number, message, can_id_key, can_id_counter, nonce):
     # Key derivation function
     salt = anchor_random_number
     kdf= PBKDF2HMAC(
@@ -18,9 +19,9 @@ def sendCanFrame(anchor_random_number, message, can_id_key, can_id_counter, can_
         backend = default_backend()
     )
     kdf_output = kdf.derive(can_id_key)
-    block_cipher = AES.new(can_id_key, AES.MODE_CTR, counter = can_id_initial_vector)
+    ctr_counter = Counter.new(120, nonce) #counter size will be 120 bits + *nonce size* bits. Counter size should be as big as block size (https://pythonhosted.org/pycrypto/Crypto.Cipher.blockalgo-module.html#MODE_CTR)
+    block_cipher = AES.new(can_id_key, AES.MODE_CTR, counter = ctr_counter)
     derived_key = block_cipher.encrypt(kdf_output)
-#    print("derived_key_sending: " + str("".join("\\x%02x" % i for i in derived_key)))
     
     # Authentication
     hmac = HMAC.new(kdf_output, digestmod = SHA256)
@@ -28,14 +29,18 @@ def sendCanFrame(anchor_random_number, message, can_id_key, can_id_counter, can_
     authentication = hmac.hexdigest().encode()[:8]
     
     # Encryption
-    frame = message + can_id_counter + authentication
-#    print("frame_sending: " + str("".join("\\x%02x" % i for i in frame)))
-#    print("frame_length: "+ str(len(frame)))
-    ciphertext = xor(frame, derived_key)
-#    print("cipher_text_sending: " + str("".join("\\x%02x" % i for i in ciphertext)))
+    data_frame = message + can_id_counter + authentication
+    ciphertext = xor(data_frame, derived_key)
+    
     return ciphertext
+   
+def getCanID(can_frame):
+    can_id = can_frame[1:11]
+    return can_id
 
-def receiveCanFrame(anchor_random_number, can_id_key, can_id_initial_vector, ciphertext):
+
+
+def generateAnchorCanData(anchor_random_number, message, can_id_key, can_id_counter, nonce):
     # Key derivation function
     salt = anchor_random_number
     kdf= PBKDF2HMAC(
@@ -46,22 +51,50 @@ def receiveCanFrame(anchor_random_number, can_id_key, can_id_initial_vector, cip
         backend = default_backend()
     )
     kdf_output = kdf.derive(can_id_key)
-    block_cipher = AES.new(can_id_key, AES.MODE_CTR, counter = can_id_initial_vector)
+    ctr_counter = Counter.new(120, nonce) #counter size will be 120 bits + *nonce size* bits. Counter size should be as big as block size (https://pythonhosted.org/pycrypto/Crypto.Cipher.blockalgo-module.html#MODE_CTR)
+    block_cipher = AES.new(can_id_key, AES.MODE_CTR, counter = ctr_counter)
     derived_key = block_cipher.encrypt(kdf_output)
-#    print("derived_key_receiving: " + str("".join("\\x%02x" % i for i in derived_key)))
-#    print("cipher_text_receiving: " + str("".join("\\x%02x" % i for i in ciphertext)))
     
-    #decryption
-    frame = xor(derived_key, ciphertext)
-#    print("frame_receiving: " + str("".join("\\x%02x" % i for i in frame)))
-#    print("frame_length: "+ str(len(frame)))
+    # Authentication
     hmac = HMAC.new(kdf_output, digestmod = SHA256)
-    message = frame[:54]
-    counter = frame[54:56]
+    hmac.update(message + can_id_counter + anchor_random_number)
+    authentication = hmac.hexdigest().encode()[:8]
+    
+    # Encryption
+    data_frame = message + can_id_counter + authentication
+    ciphertext = xor(data_frame, derived_key)
+    
+    return ciphertext
+
+def verifyAnchorCanData(anchor_random_number, can_id_key, nonce, ciphertext):
+    # Key derivation function
+    salt = anchor_random_number
+    kdf= PBKDF2HMAC(
+        algorithm = hashes.SHA256(),
+        length = 64,
+        salt = salt,
+        iterations = 10000,
+        backend = default_backend()
+    )
+    kdf_output = kdf.derive(can_id_key)
+    ctr_counter = Counter.new(120, nonce) #counter size will be 120 bits + *nonce size* bits. Counter size should be as big as block size (https://pythonhosted.org/pycrypto/Crypto.Cipher.blockalgo-module.html#MODE_CTR)
+    block_cipher = AES.new(can_id_key, AES.MODE_CTR, counter = ctr_counter)
+    derived_key = block_cipher.encrypt(kdf_output)
+    
+    # Decryption
+    data_frame = xor(derived_key, ciphertext)
+    message = data_frame[:54]
+    counter = data_frame[54:56]
+    received_hash = data_frame[56:64]
+    
+    # Authentication check
+    hmac = HMAC.new(kdf_output, digestmod = SHA256)
     hmac.update(message + counter + anchor_random_number)
     authentication = hmac.hexdigest().encode()[:8]
     
-    if  authentication == frame[56:64]:
+    #TODO: add Ta (counter) check
+    
+    if  authentication == received_hash:
         print("\nSuccesful authentication, message is: " + str(message))
     else:
         print("\nFailed authentication, can't retrieve message!")
@@ -69,18 +102,28 @@ def receiveCanFrame(anchor_random_number, can_id_key, can_id_initial_vector, cip
 
 # Example code to use the functions
 
-#sendCanFrame(anchor_random_number, message, can_id_key, can_id_counter, can_id_initial_vector)
-anchor_random_number = get_random_bytes(64)
+#generateAnchorCanData(anchor_random_number, message, can_id_key, can_id_counter, can_id_initial_vector)
+anchor_random_number = get_random_bytes(56)
 print("random_number: " + str("".join("\\x%02x" % i for i in anchor_random_number))) # display bytes
 message = b'thisisthemessagethisisthemessagethisisthemessagethisis'
 print("message: " + str("".join("\\x%02x" % i for i in message))) # display bytes
 can_id_key = b'thisisjustakeeeythisisjustakeeey'
 print("can_id_key: " + str("".join("\\x%02x" % i for i in can_id_key))) # display bytes
-can_id_counter = b'01'
+can_id_counter = b'01' #must be incremented
 print("can_id_counter: " + str("".join("\\x%02x" % i for i in can_id_counter))) # display bytes
-can_id_initial_vector= Counter.new(128)
-ciphertext = sendCanFrame(anchor_random_number, message, can_id_key, can_id_counter, can_id_initial_vector)
+initial_vector = get_random_bytes(1)
+nonce = initial_vector
+ciphertext = generateAnchorCanData(anchor_random_number, message, can_id_key, can_id_counter, nonce)
 
-#receiveCanFrame(anchor_random_number, can_id_key, can_id_initial_vector, ciphertext)
-can_id_initial_vector= Counter.new(128)
-receiveCanFrame(anchor_random_number, can_id_key, can_id_initial_vector, ciphertext)
+#verifyAnchorCanData(anchor_random_number, can_id_key, can_id_initial_vector, ciphertext)
+can_id_key = b'thisisjustakeeeythisisjustakeeey'
+can_id_counter = b'01' #must be incremented
+verifyAnchorCanData(anchor_random_number, can_id_key, nonce, ciphertext)
+
+#
+bustype = 'socketcan'
+channel = 'vcan0'
+bus = can.interface.Bus(channel=channel, bustype=bustype)
+for i in range(10):
+    msg = can.Message(arbitration_id = 0xc0ffee, data = [id, i, 0, 1, 3, 1, 4, 1], is_extended_id = False, check = True)
+    bus.send(msg)
